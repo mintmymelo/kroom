734d485c1d18fe6a5f40d10e6f0d6587bfefddf6
Jaruwit Sriburanasorn <Jaruwit@Jaruwits-MacBook-Pro.local>
2017-01-19 14:01:53 +0700

    fix token

---
+++ KroomPractice/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  KroomPractice
+//
+//  Created by Jaruwit Sriburanasorn on 1/19/2560 BE.
+//  Copyright © 2560 Penpitcha Natisupalak. All rights reserved.
+//
+
+import Foundation
+
+class Kroom {
+    static let shared = Kroom()
+    private init() {}
+    let loginURL = "http://10.215.101.76:5000/user/login"
+    let logoutURL = "http://10.215.101.76:5000/user/logout"
+}
