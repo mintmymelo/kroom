d8b554ce5f2f8595e6b962a3d4e5d52264c7ee74
Penpitcha Natisupalak <penpitchan@Penpitchas-MacBook-Pro.local>
2017-01-19 15:49:24 +0700

    Updated UI -> Book, show + ตรึง Login+Menu

--- KroomPractice/BookingViewController.swift
+++ KroomPractice/BookingViewController.swift
@@ -11,26 +11,40 @@ import FSCalendar
 
 class BookingViewController: UIViewController {
 
+    @IBOutlet weak var calendar: FSCalendar!
+
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var innerView: UIView!
     @IBOutlet weak var containerHeight: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        scrollView.backgroundColor = UIColor.lightGray
+        //scrollView.backgroundColor = UIColor.lightGray
+        //configureCalendar
     }
     
     override func viewDidLayoutSubviews() {
-        containerHeight.constant = 670.0+230.0
+        containerHeight.constant = 890.0
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+
+    private func configureCalendar() {
+        self.calendar.delegate = self
+        self.calendar.dataSource = self
+        self.calendar.locale = Locale(identifier: "en-US")
+        self.calendar.appearance.adjustsFontSizeToFitContentSize = false
+        self.calendar.appearance.titleFont = UIFont(name: "Helvetica Neue", size: 16.0)
+        self.calendar.appearance.titleSelectionColor = UIColor.black
+        self.calendar.appearance.selectionColor = UIColor.clear
+        //self.calendar.appearance.borderSelectionColor = UIColor.greenKBankColor
+        self.calendar.delegate?.calendarCurrentPageDidChange!(calendar)
+        self.calendar.delegate?.calendar!(calendar, didSelect: calendar.today!, at: .current)
+        self.calendar.select(calendar.today!, scrollToDate: true)
     }
     
-
     /*
     // MARK: - Navigation
 
@@ -41,4 +55,15 @@ class BookingViewController: UIViewController {
     }
     */
 
+}
+
+
+extension BookingViewController: FSCalendarDelegate {
+    func calendar(_ calendar: FSCalendar, didDeselect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        print(date)
+    }
+}
+
+extension BookingViewController: FSCalendarDataSource {
+    
 }
